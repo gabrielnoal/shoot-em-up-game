0113bcbd60e0cdd0e24cbadfd7c763edc9716e28
so-os-troxa <94878659+so-os-troxa@users.noreply.github.com>
2021-11-22 22:39:25 -0300

    Update ConditionDistLT.cs

--- Shoot'em Up/Assets/_Scripts/ConditionDistLT.cs
+++ Shoot'em Up/Assets/_Scripts/ConditionDistLT.cs
@@ -9,13 +9,4 @@ public class ConditionDistLT : Condition
     float maxDist;
 
     public ConditionDistLT(Transform _agent, Transform _target, float _maxDist) {
-        agent = _agent;
-        target = _target;
-        maxDist = _maxDist;
-    }
 
-    public override bool Test() {
-        return Vector2.Distance(agent.position, target.position) <= maxDist;
-    }
-
-}
